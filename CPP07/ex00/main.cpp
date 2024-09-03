32095deb96e5df5743b331866af864c39f5ac5e0
Ismael López Ramírez <133966465+ilopez-r@users.noreply.github.com>
2024-09-03 16:21:10 +0200

    Add files via upload

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ilopez-r <ilopez-r@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 14:35:28 by ilopez-r          #+#    #+#             */
-/*   Updated: 2024/08/30 14:48:18 by ilopez-r         ###   ########.fr       */
+/*   Updated: 2024/09/03 16:16:54 by ilopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ int main( void )
 {
 	int a = 2;
 	int b = 3;
-	::swap( a, b );
+	swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
-	::swap(c, d);
+	swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,59 +6,106 @@
 /*   By: ilopez-r <ilopez-r@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 16:16:16 by ilopez-r          #+#    #+#             */
-/*   Updated: 2024/08/30 18:36:41 by ilopez-r         ###   ########.fr       */
+/*   Updated: 2024/09/03 16:14:21 by ilopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
 #include "Array.hpp"
 
-#define MAX_VAL 750
-int main(int, char**)
+int main(void)
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
+    std::cout << "-        [test with strings]        -" << std::endl;
+    Array<std::string> strs(2);
     try
     {
-        numbers[-2] = 0;
+        strs[0] = "Hello";
+        strs[1] = "World!";
+        
+        std::cout << "strs[0]: " << strs[0] << std::endl;
+        std::cout << "strs[1]: " << strs[1] << std::endl;
+        std::cout << "Array size is: " << strs.size() << std::endl;
+
+        std::cout << "Attempting to access strs[3]: ";
+        std::cout << strs[3] << std::endl;
     }
     catch(const std::exception& e)
     {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
+        std::cerr << e.what() << "\n";
     }
 
-    for (int i = 0; i < MAX_VAL; i++)
+    std::cout << "-        [test with ints]        -" << std::endl;
+    try
     {
-        numbers[i] = rand();
+        Array<int> ints(3);
+        ints[0] = 42;
+        ints[1] = 28;
+        ints[2] = 16;
+        
+        std::cout << "ints[0]: " << ints[0] << std::endl;
+        std::cout << "ints[1]: " << ints[1] << std::endl;
+        std::cout << "ints[2]: " << ints[2] << std::endl;
+        std::cout << "Array size is: " << ints.size() << std::endl;
+
+        std::cout << "Attempting to access ints[5]: ";
+        std::cout << ints[5] << std::endl;
     }
-    delete [] mirror;//
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << "\n";
+    }
+
+    std::cout << "-        [test with chars]        -" << std::endl;
+    try
+    {
+        Array<char> chars(5);
+        chars[0] = 'A';
+        chars[1] = 'B';
+        chars[2] = 'C';
+        chars[3] = 'D';
+        chars[4] = 'E';
+
+        for (unsigned int i = 0; i < chars.size(); ++i)
+        {
+            std::cout << "chars[" << i << "]: " << chars[i] << std::endl;
+        }
+        std::cout << "Array size is: " << chars.size() << std::endl;
+
+        std::cout << "Attempting to access chars[7]: ";
+        std::cout << chars[7] << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << "\n";
+    }
+
+    std::cout << "-        [test with copy constructor]        -" << std::endl;
+    try
+    {
+        Array<int> original(3);
+        original[0] = 1;
+        original[1] = 2;
+        original[2] = 3;
+
+        Array<int> copy(original);
+
+        std::cout << "Original array: ";
+        for (unsigned int i = 0; i < original.size(); ++i)
+        {
+            std::cout << original[i] << " ";
+        }
+        std::cout << std::endl;
+
+        std::cout << "Copied array: ";
+        for (unsigned int i = 0; i < copy.size(); ++i)
+        {
+            std::cout << copy[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << "\n";
+    }
+
     return 0;
 }
